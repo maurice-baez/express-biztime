f49612be1fa6924fcf86398a0d0dd192d493d2ce
Maurice <maurice.baez1@gmail.com>
2022-04-14 12:04:27 -0700

    added git company by code route

--- routes/companies.js
+++ routes/companies.js
@@ -1,17 +1,39 @@
 const express = require("express");
+const res = require("express/lib/response");
+const { NotFoundError, BadRequestError } = require("../expressError");
+
 
 const db = require("../db");
 const router = new express.Router();
 
-/** GET /users: get list of companies */
+/** GET /companies: get list of companies */
 router.get("/", async function (req, res) {
   const results = await db.query(
-    'SELECT code, name, description FROM companies'
-  )
+    'SELECT code, name FROM companies'
+  );
 
   const companies = results.rows;
   return res.json({companies});
 });
+
+/** GET /companies/code: get an object of company information for requested company */
+router.get("/:code", async function (req, res){
+  const code = req.params.code;
+
+  const results = await db.query(
+    `SELECT code, name, description FROM companies
+    WHERE code = $1`, [code]);
+  const [company] = results.rows;
+
+  return res.json({company});
+  });
+
+
+
+
+
+
+
 
 /** DELETE /users/[id]: delete user, return {message: Deleted} */
 router.delete("/:id", function (req, res) {
